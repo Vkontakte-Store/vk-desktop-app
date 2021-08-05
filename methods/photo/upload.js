85b4249232430ed3789fe743f09b29325c18bdae
alexstep <alex.step.kellas@gmail.com>
2021-08-05 18:03:31 +0300

    Upload files from local filesystem

--- methods/photo/upload.js
+++ methods/photo/upload.js
@@ -3,17 +3,31 @@ const FormData = require('form-data')
 const imgSize = require('image-size')
 const resizeImg = require('resize-img')
 const scraper = require('./scrapper')
-
+const fs = require('fs')
+const path = require('path')
 
 // Find img on web page and download
 async function getImage(url){
-    const response = await fetch(url)
+    const link = new URL(url)
+    
+    // local filesystem
+    if (link.protocol === 'file:'){
+        return new Promise(function(resolve){
+            fs.readFile(path.resolve(link.pathname), (err, localImage)=>{
+                if (err) reject(err)
+                resolve(localImage)
+            })
+        })
+    }
+
+    // download from web
+    const response = await fetch(link.href)
     const type = response.headers.get('Content-Type')
     if (type.includes('image/') || !type.includes('text/html'))
         return Buffer.from(await response.arrayBuffer())
 
-    const { image } = scraper( url, await response.text() )
-
+    // parse html
+    const { image } = scraper( link.href, await response.text() )
     if (!image)
         return null
 
@@ -22,13 +36,12 @@ async function getImage(url){
 
 // Resize image if necessary
 async function resizeImage(img){
-    const info = imgSize(img)
-    console.log('size', info);
-    const size = {}
-    if (info.width < 500) size.width = 500
-    if (info.height < 500) size.height = 500
-    if (Object.keys(size).length){
-        return resizeImg(img, size)
+    const size = imgSize(img)
+    const newSize = {}
+    if (size.width < 500) newSize.width = 500
+    if (size.height < 500) newSize.height = 500
+    if (Object.keys(newSize).length){
+        return resizeImg(img, newSize)
     }
 
     return img
